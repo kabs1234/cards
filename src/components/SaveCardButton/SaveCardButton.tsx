d671daaa730412ee1df3d24aa46a24a6b7433a09
Aikhan Zhashkeyev <spy.04@mail.ru>
2025-08-27 14:17:37 +0500

    FEAT: implement darken background on card actions feature

--- src/components/SaveCardButton/SaveCardButton.tsx
+++ src/components/SaveCardButton/SaveCardButton.tsx
@@ -9,15 +9,18 @@ export default function SaveCardButton({
   card,
   isFormValidated,
   onCardSuccesfulSave,
+  setIsActionLoading,
 }: {
   card: CardFormType;
   isFormValidated: boolean;
   onCardSuccesfulSave: () => void;
+  setIsActionLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }): ReactElement {
   const [saveCard, { isLoading }] = useSaveCardMutation();
 
   const tryToSaveCard = async () => {
     try {
+      setIsActionLoading(true);
       await saveCard(card).unwrap();
       toast.success('Card was created successfully!');
       onCardSuccesfulSave();
@@ -26,6 +29,8 @@ export default function SaveCardButton({
         'Unexpected error occured. Please try to save the card again'
       );
       throw err;
+    } finally {
+      setIsActionLoading(false);
     }
   };
 
